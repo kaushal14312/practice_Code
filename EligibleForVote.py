e40bbec0e30bc57e621a0305b805fc613b5eaecd
kaushal14312 <kaushalraj11a@gmail.com>
2024-12-02 00:37:22 +0530

    01-12-2024

---
+++ EligibleForVote.py
@@ -0,0 +1,6 @@
+number=int(input("Enter you age"))
+
+if number>=18:
+    print("You are eligible to Vote")
+else:
+    print("You are not eligible to vote")
---
+++ ProgramToaddition.py
@@ -0,0 +1,10 @@
+number1=int(input("Enter first number: "))
+number2=int(input("Entert Second number: "))
+print(number1+number2)
+
+#factorial of a number
+number=int(input("Enter the number"))
+factorial=1
+for i in range(1,number+1):
+        factorial=factorial*i
+print(factorial)
